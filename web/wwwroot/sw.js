62499d0b714bf59fe5b2d6db5f20231091603515
Edin Ćehić <edinehi53@gmail.com>
2023-01-10 14:04:57 +0100

    service worker now only installs doesnt cache

--- web/wwwroot/sw.js
+++ web/wwwroot/sw.js
@@ -1,26 +1,13 @@
 self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open('sloveniatrips-cache')
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll([
-            '/',
-          ]);
-        })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
-    );
-  });
+  event.waitUntil(
+      self.skipWaiting()
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
